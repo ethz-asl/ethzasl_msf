e101a779bff4f21da8efd829e355cbb775c38828
Simon Lynen <simon.lynen@designated.de>
2012-11-06 19:05:00 +0100

    moved tmp and all other definitions to separate header files

---
+++ msf_core/include/msf_core/msf_fwd.hpp
@@ -0,0 +1,17 @@
+/*
+ * msf_fwd.hpp
+ *
+ *  Created on: Nov 6, 2012
+ *      Author: slynen
+ */
+
+#ifndef MSF_FWD_HPP_
+#define MSF_FWD_HPP_
+
+//some fwd decls
+
+//state variable
+template<typename type_T, int name_T>
+struct StateVar_T;
+
+#endif /* MSF_FWD_HPP_ */
---
+++ msf_core/include/msf_core/msf_statedef.hpp
@@ -0,0 +1,35 @@
+/*
+ * msf_statedef.hpp
+ *
+ *  Created on: Nov 6, 2012
+ *      Author: slynen
+ */
+
+#ifndef MSF_STATEDEF_HPP_
+#define MSF_STATEDEF_HPP_
+
+namespace msf_core{
+namespace{
+
+enum{ //must not manually set the enum values!
+	p_ci,
+	q_ci,
+	q_wv,
+	L
+};
+
+//setup core state, then auxiliary state
+typedef boost::fusion::vector<
+		StateVar_T<Eigen::Vector3d, p_ci >,
+		StateVar_T<Eigen::Quaterniond, q_ci >,
+		StateVar_T<Eigen::Quaterniond, q_wv >,
+		StateVar_T<double, L >
+> fullState_T;
+}
+
+typedef EKFState_T<msf_core::fullState_T> EKFState;
+
+}
+
+
+#endif /* MSF_STATEDEF_HPP_ */
