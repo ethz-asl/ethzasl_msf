fa08cd3933a9fc7e7839ba167bdb03aa69fffffc
Simon Lynen <simon.lynen@mavt.ethz.ch>
2013-03-01 19:37:58 +0100

    added position msf_ compiles, untested

---
+++ msf_updates/include/msf_updates/position_measurement/position_measurement.h
@@ -0,0 +1,258 @@
+/*
+
+ Copyright (c) 2010, Stephan Weiss, ASL, ETH Zurich, Switzerland
+ You can contact the author at <stephan dot weiss at ieee dot org>
+ Copyright (c) 2012, Simon Lynen, ASL, ETH Zurich, Switzerland
+ You can contact the author at <slynen at ethz dot ch>
+ Copyright (c) 2012, Markus Achtelik, ASL, ETH Zurich, Switzerland
+ You can contact the author at <acmarkus at ethz dot ch>
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of ETHZ-ASL nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL ETHZ-ASL BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+#ifndef POSITION_MEASUREMENT_HPP_
+#define POSITION_MEASUREMENT_HPP_
+
+#include <msf_core/msf_measurement.h>
+#include <msf_core/msf_core.h>
+#include <msf_updates/PoseDistorter.h>
+
+namespace msf_updates{
+namespace position_measurement{
+enum
+{
+  nMeasurements = 3
+};
+/**
+ * \brief a measurement as provided by a pose tracking algorithm
+ */
+typedef msf_core::MSF_Measurement<geometry_msgs::PointStamped, nMeasurements, msf_updates::EKFState> PositionMeasurementBase;
+struct PositionMeasurement : public PositionMeasurementBase
+{
+private:
+  typedef PositionMeasurementBase Measurement_t;
+  typedef Measurement_t::Measurement_ptr measptr_t;
+
+  virtual void makeFromSensorReadingImpl(measptr_t msg)
+  {
+
+    Eigen::Matrix<double, nMeasurements, msf_core::MSF_Core<msf_updates::EKFState>::nErrorStatesAtCompileTime> H_old;
+    Eigen::Matrix<double, nMeasurements, 1> r_old;
+
+    H_old.setZero();
+    R_.setZero(); //TODO:remove later, already done in ctor of base
+
+    // get measurement
+    z_p_ = Eigen::Matrix<double, 3, 1>(msg->point.x, msg->point.y, msg->point.z);
+
+//    if (fixed_covariance_)//  take fix covariance from reconfigure GUI
+//    {
+
+      const double s_zp = n_zp_ * n_zp_;
+      R_ = (Eigen::Matrix<double, nMeasurements, 1>() << s_zp, s_zp, s_zp).finished().asDiagonal();
+
+//    }else{// take covariance from sensor
+//
+//      R_.block<3, 3>(0, 0) = Eigen::Matrix<double, 3, 3>(&msg->covariance[0]);
+//
+//      if(msg->header.seq % 100 == 0){ //only do this check from time to time
+//        if(R_.block<3, 3>(0, 0).determinant() < 0)
+//          ROS_WARN_STREAM_THROTTLE(60,"The covariance matrix you provided for the position sensor is not positive definite");
+//      }
+//    }
+  }
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  Eigen::Matrix<double, 3, 1> z_p_; /// position measurement
+  double n_zp_; /// position measurement noise
+
+  bool fixed_covariance_;
+
+  typedef msf_updates::EKFState EKFState_T;
+  typedef EKFState_T::StateSequence_T StateSequence_T;
+  typedef EKFState_T::StateDefinition_T StateDefinition_T;
+  virtual ~PositionMeasurement()
+  {
+  }
+  PositionMeasurement(double n_zp, bool fixed_covariance, bool isabsoluteMeasurement, int sensorID) :
+    PositionMeasurementBase(isabsoluteMeasurement, sensorID),
+    n_zp_(n_zp), fixed_covariance_(fixed_covariance)
+  {
+  }
+  virtual std::string type(){
+    return "position";
+  }
+
+  virtual void calculateH(boost::shared_ptr<EKFState_T> state_in, Eigen::Matrix<double, nMeasurements, msf_core::MSF_Core<EKFState_T>::nErrorStatesAtCompileTime>& H){
+    const EKFState_T& state = *state_in; //get a const ref, so we can read core states
+
+    H.setZero();
+
+    // get rotation matrices
+    Eigen::Matrix<double, 3, 3> C_q = state.get<StateDefinition_T::q>().conjugate().toRotationMatrix();
+
+    // preprocess for elements in H matrix
+
+    Eigen::Matrix<double, 3, 3> p_prism_imu_sk = skew(state.get<StateDefinition_T::p_prism_imu>());
+
+    //get indices of states in error vector
+    enum{
+      idxstartcorr_p_ = msf_tmp::getStartIndexInCorrection<StateSequence_T, StateDefinition_T::p>::value,
+      idxstartcorr_v_ = msf_tmp::getStartIndexInCorrection<StateSequence_T, StateDefinition_T::v>::value,
+      idxstartcorr_q_ = msf_tmp::getStartIndexInCorrection<StateSequence_T, StateDefinition_T::q>::value,
+      idxstartcorr_p_prism_imu_ = msf_tmp::getStartIndexInCorrection<StateSequence_T, StateDefinition_T::p_prism_imu>::value,
+    };
+
+    // construct H matrix using H-blockx :-)
+    // position:
+    H.block<3, 3>(0, idxstartcorr_p_) = Eigen::Matrix<double, 3, 3>::Identity(); // p
+
+    H.block<3, 3>(0, idxstartcorr_q_) = - C_q.transpose() * p_prism_imu_sk; // q
+
+    H.block<3, 3>(0, idxstartcorr_p_prism_imu_) = C_q.transpose(); //p_ci
+
+    // attitude
+    H.block<3, 3>(3, idxstartcorr_q_) = Eigen::Matrix<double, 3, 3>::Identity(); // q
+  }
+
+  /**
+   * the method called by the msf_core to apply the measurement represented by this object
+   */
+  virtual void apply(boost::shared_ptr<EKFState_T> state_nonconst_new, msf_core::MSF_Core<EKFState_T>& core)
+  {
+
+    if(isabsolute_){//does this measurement refer to an absolute measurement, or is is just relative to the last measurement
+      const EKFState_T& state = *state_nonconst_new; //get a const ref, so we can read core states
+      // init variables
+      Eigen::Matrix<double, nMeasurements, msf_core::MSF_Core<EKFState_T>::nErrorStatesAtCompileTime> H_new;
+      Eigen::Matrix<double, nMeasurements, 1> r_old;
+
+      calculateH(state_nonconst_new, H_new);
+
+      // get rotation matrices
+      Eigen::Matrix<double, 3, 3> C_q = state.get<StateDefinition_T::q>().conjugate().toRotationMatrix();
+
+      // construct residuals
+      // position
+      r_old.block<3, 1>(0, 0) = z_p_ - (state.get<StateDefinition_T::p>() + C_q.transpose() * state.get<StateDefinition_T::p_prism_imu>());
+
+      if(!checkForNumeric(r_old, "r_old")){
+        ROS_ERROR_STREAM("r_old: "<<r_old);
+        ROS_WARN_STREAM("state: "<<const_cast<EKFState_T&>(state).toEigenVector().transpose());
+      }
+      if(!checkForNumeric(H_new, "H_old")){
+        ROS_ERROR_STREAM("H_old: "<<H_new);
+        ROS_WARN_STREAM("state: "<<const_cast<EKFState_T&>(state).toEigenVector().transpose());
+      }
+      if(!checkForNumeric(R_, "R_")){
+        ROS_ERROR_STREAM("R_: "<<R_);
+        ROS_WARN_STREAM("state: "<<const_cast<EKFState_T&>(state).toEigenVector().transpose());
+      }
+
+      // call update step in base class
+      this->calculateAndApplyCorrection(state_nonconst_new, core, H_new, r_old, R_);
+
+    }else{
+
+      // init variables
+      //get previous measurement
+      boost::shared_ptr<msf_core::MSF_MeasurementBase<EKFState_T> > prevmeas_base = core.getPreviousMeasurement(this->time, this->sensorID_);
+
+      if(prevmeas_base->time == -1){
+        ROS_WARN_STREAM("The previous measurement is invalid. Could not apply measurement");
+        return;
+      }
+      //try to make this a pose measurement
+      boost::shared_ptr<PositionMeasurement> prevmeas = boost::shared_dynamic_cast<PositionMeasurement>(prevmeas_base);
+      if(!prevmeas){
+        ROS_WARN_STREAM("The dynamic cast of the previous measurement has failed. Could not apply measurement");
+        return;
+      }
+
+      //get state at previous measurement
+      boost::shared_ptr<EKFState_T> state_nonconst_old = core.getStateAtTime(prevmeas->time);
+
+      if(state_nonconst_old->time == -1){
+        ROS_WARN_STREAM("The state at the previous measurement is invalid. Could not apply measurement");
+        return;
+      }
+
+      const EKFState_T& state_new = *state_nonconst_new; //get a const ref, so we can read core states
+      const EKFState_T& state_old = *state_nonconst_old; //get a const ref, so we can read core states
+
+      Eigen::Matrix<double, nMeasurements, msf_core::MSF_Core<EKFState_T>::nErrorStatesAtCompileTime> H_new, H_old;
+      Eigen::Matrix<double, nMeasurements, 1> r_new, r_old;
+
+      calculateH(state_nonconst_old, H_old);
+
+      H_old *= -1;
+
+      calculateH(state_nonconst_new, H_new);
+
+      //TODO check that both measurements have the same states fixed!
+
+      Eigen::Matrix<double, 3, 3> C_wv_old, C_wv_new;
+      Eigen::Matrix<double, 3, 3> C_q_old, C_q_new;
+
+      C_q_new = state_new.get<StateDefinition_T::q>().conjugate().toRotationMatrix();
+      C_q_old = state_old.get<StateDefinition_T::q>().conjugate().toRotationMatrix();
+
+      // construct residuals
+      // position
+      //TODO reenable p_vw
+      Eigen::Matrix<double, 3, 1> diffprobpos = (state_new.get<StateDefinition_T::p>()
+          + C_q_new.transpose() * state_new.get<StateDefinition_T::p_prism_imu>())
+          - (state_old.get<StateDefinition_T::p>()
+              + C_q_old.transpose() * state_old.get<StateDefinition_T::p_prism_imu>());
+
+      Eigen::Matrix<double, 3, 1> diffmeaspos = z_p_ - prevmeas->z_p_;
+
+      r_new.block<3, 1>(0, 0) = diffmeaspos - diffprobpos;
+
+      if(!checkForNumeric(r_old, "r_old")){
+        ROS_ERROR_STREAM("r_old: "<<r_old);
+        ROS_WARN_STREAM("state: "<<const_cast<EKFState_T&>(state_new).toEigenVector().transpose());
+      }
+      if(!checkForNumeric(H_new, "H_old")){
+        ROS_ERROR_STREAM("H_old: "<<H_new);
+        ROS_WARN_STREAM("state: "<<const_cast<EKFState_T&>(state_new).toEigenVector().transpose());
+      }
+      if(!checkForNumeric(R_, "R_")){
+        ROS_ERROR_STREAM("R_: "<<R_);
+        ROS_WARN_STREAM("state: "<<const_cast<EKFState_T&>(state_new).toEigenVector().transpose());
+      }
+
+      // call update step in base class
+      this->calculateAndApplyCorrectionRelative(state_nonconst_old, state_nonconst_new, core, H_old, H_new, r_new, R_);
+
+    }
+  }
+};
+
+}
+}
+#endif /* POSITION_MEASUREMENT_HPP_ */
---
+++ msf_updates/src/position_msf/position_sensormanager.h
@@ -0,0 +1,190 @@
+/*
+
+Copyright (c) 2010, Stephan Weiss, ASL, ETH Zurich, Switzerland
+You can contact the author at <stephan dot weiss at ieee dot org>
+Copyright (c) 2012, Simon Lynen, ASL, ETH Zurich, Switzerland
+You can contact the author at <slynen at ethz dot ch>
+ Copyright (c) 2012, Markus Achtelik, ASL, ETH Zurich, Switzerland
+ You can contact the author at <acmarkus at ethz dot ch>
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+ * Neither the name of ETHZ-ASL nor the
+names of its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL ETHZ-ASL BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+#ifndef POSITION_MEASUREMENTMANAGER_H
+#define POSITION_MEASUREMENTMANAGER_H
+
+#include <ros/ros.h>
+
+#include <msf_core/msf_core.h>
+#include <msf_core/msf_sensormanagerROS.h>
+#include "msf_statedef.hpp"
+#include <msf_updates/position_measurement/position_sensorhandler.h>
+#include <msf_updates/position_measurement/position_measurement.h>
+#include <msf_updates/SinglePositionSensorConfig.h>
+
+namespace msf_position_sensor{
+
+typedef msf_updates::SinglePositionSensorConfig Config_T;
+typedef dynamic_reconfigure::Server<Config_T> ReconfigureServer;
+typedef boost::shared_ptr<ReconfigureServer> ReconfigureServerPtr;
+
+class PositionSensorManager : public msf_core::MSF_SensorManagerROS<msf_updates::EKFState>
+{
+  typedef PositionSensorHandler<msf_updates::position_measurement::PositionMeasurement, PositionSensorManager> PositionSensorHandler_T;
+  friend PositionSensorHandler_T;
+public:
+  typedef msf_updates::EKFState EKFState_T;
+  typedef EKFState_T::StateSequence_T StateSequence_T;
+  typedef EKFState_T::StateDefinition_T StateDefinition_T;
+
+  PositionSensorManager(ros::NodeHandle pnh = ros::NodeHandle("~/position_sensor"))
+  {
+    bool positionabsolute = true; ///<does the position sensor provides absolute measurements : TODO read from parameters which are specific to this sensor
+
+    position_handler_.reset(new PositionSensorHandler_T(*this, positionabsolute));
+    addHandler(position_handler_);
+
+    reconf_server_.reset(new ReconfigureServer(pnh));
+    ReconfigureServer::CallbackType f = boost::bind(&PositionSensorManager::config, this, _1, _2);
+    reconf_server_->setCallback(f);
+  }
+  virtual ~PositionSensorManager(){}
+
+  virtual const Config_T& getcfg(){
+    return config_;
+  }
+
+private:
+  boost::shared_ptr<PositionSensorHandler_T> position_handler_;
+
+  Config_T config_;
+  ReconfigureServerPtr reconf_server_; ///< dynamic reconfigure server
+
+  /**
+   * \brief dynamic reconfigure callback
+   */
+  virtual void config(Config_T &config, uint32_t level){
+    config_ = config;
+    position_handler_->setNoises(config.noise_position);
+    position_handler_->setDelay(config.delay);
+    if((level & msf_updates::SinglePositionSensor_INIT_FILTER) && config.init_filter == true){
+      init(1.0);
+      config.init_filter = false;
+    }
+  }
+
+  void init(double scale)
+  {
+    Eigen::Matrix<double, 3, 1> p, v, b_w, b_a, g, w_m, a_m, p_prism_imu, p_vc;
+    Eigen::Quaternion<double> q, q_cv;
+    msf_core::MSF_Core<EKFState_T>::ErrorStateCov P;
+
+    // init values
+    g << 0, 0, 9.81;	        /// gravity
+    b_w << 0,0,0;		/// bias gyroscopes
+    b_a << 0,0,0;		/// bias accelerometer
+
+    v << 0,0,0;			/// robot velocity (IMU centered)
+    w_m << 0,0,0;		/// initial angular velocity
+    a_m = g;			/// initial acceleration
+
+    P.setZero(); // error state covariance; if zero, a default initialization in msf_core is used
+
+    p_vc = position_handler_->getPositionMeasurement();
+
+    ROS_INFO_STREAM("initial measurement pos:["<<p_vc.transpose()<<"] orientation:["<<q_cv.w()<<", "<<q_cv.x()<<", "<<q_cv.y()<<", "<<q_cv.z()<<"]");
+
+    // check if we have already input from the measurement sensor
+    if (p_vc.norm() == 0)
+      ROS_WARN_STREAM("No measurements received yet to initialize position - using [0 0 0]");
+    if ((p_vc.norm() == 1) & (q_cv.w() == 1))
+      ROS_WARN_STREAM("No measurements received yet to initialize attitude - using [1 0 0 0]");
+
+    ros::NodeHandle pnh("~");
+    pnh.param("init/p_prism_imu/x", p_prism_imu[0], 0.0);
+    pnh.param("init/p_prism_imu/y", p_prism_imu[1], 0.0);
+    pnh.param("init/p_prism_imu/z", p_prism_imu[2], 0.0);
+
+    // calculate initial attitude and position based on sensor measurements
+    q = q_cv;
+    q.normalize();
+    p = p_vc - q.toRotationMatrix() * p_prism_imu;
+
+    //prepare init "measurement"
+    boost::shared_ptr<msf_core::MSF_InitMeasurement<EKFState_T> > meas(new msf_core::MSF_InitMeasurement<EKFState_T>(true)); //hand over that we will also set the sensor readings
+
+    meas->setStateInitValue<StateDefinition_T::p>(p);
+    meas->setStateInitValue<StateDefinition_T::v>(v);
+    meas->setStateInitValue<StateDefinition_T::q>(q);
+    meas->setStateInitValue<StateDefinition_T::b_w>(b_w);
+    meas->setStateInitValue<StateDefinition_T::b_a>(b_a);
+    meas->setStateInitValue<StateDefinition_T::p_prism_imu>(p_prism_imu);
+
+    setP(meas->get_P()); //call my set P function
+    meas->get_w_m() = w_m;
+    meas->get_a_m() = a_m;
+    meas->time = ros::Time::now().toSec();
+
+    // call initialization in core
+    msf_core_->init(meas);
+  }
+
+  //prior to this call, all states are initialized to zero/identity
+  virtual void resetState(EKFState_T& state){
+
+  }
+  virtual void initState(EKFState_T& state){
+
+  }
+
+  virtual void calculateQAuxiliaryStates(EKFState_T& state, double dt){
+    const msf_core::Vector3 npicv = msf_core::Vector3::Constant(config_.noise_p_prism_imu);
+
+    //compute the blockwise Q values and store them with the states,
+    //these then get copied by the core to the correct places in Qd
+    state.getQBlock<StateDefinition_T::p_prism_imu>() = (dt * npicv.cwiseProduct(npicv)).asDiagonal();
+  }
+
+  virtual void setP(Eigen::Matrix<double, EKFState_T::nErrorStatesAtCompileTime, EKFState_T::nErrorStatesAtCompileTime>& P){
+
+    //nothing, we only use the simulated cov for the core plus diagonal for the rest
+
+  }
+
+  virtual void augmentCorrectionVector(Eigen::Matrix<double, EKFState_T::nErrorStatesAtCompileTime,1>& correction_){
+
+  }
+
+  virtual void sanityCheckCorrection(EKFState_T& delaystate, const EKFState_T& buffstate,
+                                     Eigen::Matrix<double, EKFState_T::nErrorStatesAtCompileTime,1>& correction_){
+
+
+  }
+
+};
+
+}
+#endif /* POSITION_MEASUREMENTMANAGER_H */
