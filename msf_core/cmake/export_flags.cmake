17de83b9f660c7ad6aff420c7873af59ead60992
markusachtelik <markus.achtelik@mavt.ethz.ch>
2013-08-16 14:06:06 +0200

    compile flag cleanup; msf_updates compile as release now; dynamic_reconfigure dependencies fixed-may require latest catkin version

--- msf_core/cmake/export_flags.cmake
+++ msf_core/cmake/export_flags.cmake
@@ -1,9 +1,9 @@
-add_definitions (-Wall -std=c++11 -Wno-unused-parameter -O3)# -Wextra -pedantic")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -std=c++11 -Wno-unused-parameter")# -Wextra -pedantic")
 
 # assembler on mac os doesn't know avx commands :( switch to sse4.2. assuming that our mac machines have a corei7
 IF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin") 
-  add_definitions (-msse4.2)
+  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse4.2")
 ELSE(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  add_definitions (-march=native)
+  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=native")
 ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 
