5d118514752efd8a83ffb32f4a49cec72d021420
simonlynen <simon.lynen@mavt.ethz.ch>
2013-11-06 22:24:45 -0800

    update dox

--- msf_core/include/msf_core/msf_core.h
+++ msf_core/include/msf_core/msf_core.h
@@ -16,6 +16,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
+/**
+\mainpage ethzasl_msf
+
+Time delay compensated single and multi sensor fusion framework based on an EKF. 
+Please see the wiki for more information: https://github.com/ethz-asl/ethzasl_msf/wiki
+
+The API is documented here: http://ethz-asl.github.io/ethzasl_msf
+
+You are welcome contributing to the package by opening a pull-request: 
+Please make yourself familiar with the Google c++ style guide: 
+http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml
+*/ 
+ 
 #ifndef MSF_CORE_H_
 #define MSF_CORE_H_
 
