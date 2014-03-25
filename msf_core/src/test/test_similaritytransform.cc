99346c6fc0d33c7d4b7bd45ff217076c732565a9
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-03-25 12:53:11 +0100

    Split similarity transform tests into separate test cases

--- msf_core/src/test/test_similaritytransform.cc
+++ msf_core/src/test/test_similaritytransform.cc
@@ -18,6 +18,78 @@
 #include <msf_core/testing_entrypoint.h>
 #include <msf_core/testing_predicates.h>
 
+TEST(MSF_Core, XiMatrix) {
+  using namespace msf_core;
+  Eigen::Quaterniond q(Eigen::Matrix<double, 4, 1>::Random());
+  q.normalize();
+
+  // Xi matrix test.
+  EXPECT_NEAR_EIGEN(XiMat(q.coeffs()).transpose() * q.coeffs(),
+                    (Eigen::Matrix<double, 3, 1>::Zero()),
+                    1e-5);
+}
+
+TEST(MSF_Core, GeometryMsgsCovBlockToEigen) {
+  using namespace msf_core;
+  // Block writing test.
+  similarity_transform::Pose::_covariance_type cov;
+  for (int r = 0; r < 6; r++)
+    for (int c = 0; c < 6; c++)
+      cov[r + c * 6] = r * c;
+  Eigen::Map<Matrix6> covm(cov.data());
+
+  EXPECT_NEAR_EIGEN(GeometryMsgsCovBlockToEigen(cov, geometry_msgs::cov::p,
+                                                geometry_msgs::cov::p),
+                    (covm.block<3, 3>(0, 0)), 1e-5);
+
+  EXPECT_NEAR_EIGEN(GeometryMsgsCovBlockToEigen(cov, geometry_msgs::cov::q,
+                                                geometry_msgs::cov::q),
+                    (covm.block<3, 3>(3, 3)), 1e-5);
+
+  EXPECT_NEAR_EIGEN(GeometryMsgsCovBlockToEigen(cov, geometry_msgs::cov::p,
+                                                geometry_msgs::cov::q),
+                    (covm.block<3, 3>(0, 3)), 1e-5);
+
+  EXPECT_NEAR_EIGEN(GeometryMsgsCovBlockToEigen(cov, geometry_msgs::cov::q,
+                                                geometry_msgs::cov::p),
+                    (covm.block<3, 3>(3, 0)), 1e-5);
+}
+
+TEST(MSF_Core, EigenCovBlockToGeometryMsgs) {
+  using namespace msf_core;
+  Matrix3 covp;
+  covp << 31, 32, 33, 32, 34, 35, 33, 35, 36;
+  Matrix3 covq;
+  covq << 21, 22, 23, 22, 24, 25, 23, 25, 26;
+  Matrix3 covpq;
+  covpq << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+
+  similarity_transform::Pose::_covariance_type cov;
+  for (int r = 0; r < 6; r++)
+    for (int c = 0; c < 6; c++)
+      cov[r + c * 6] = r * c;
+  Eigen::Map<Matrix6> covm(cov.data());
+
+  EigenCovBlockToGeometryMsgs(cov, covq, geometry_msgs::cov::q,
+                              geometry_msgs::cov::q);
+  EigenCovBlockToGeometryMsgs(cov, covp, geometry_msgs::cov::p,
+                              geometry_msgs::cov::p);
+
+  EigenCovBlockToGeometryMsgs(cov, covpq, geometry_msgs::cov::p,
+                              geometry_msgs::cov::q);
+
+  EigenCovBlockToGeometryMsgs(cov, covpq.transpose(), geometry_msgs::cov::q,
+                              geometry_msgs::cov::p);
+
+  EXPECT_NEAR_EIGEN((covm.block<3, 3>(0, 0)), covp, 1e-5);
+
+  EXPECT_NEAR_EIGEN((covm.block<3, 3>(3, 3)), covq, 1e-5);
+
+  EXPECT_NEAR_EIGEN((covm.block<3, 3>(0, 3)), covpq, 1e-5);
+
+  EXPECT_NEAR_EIGEN((covm.block<3, 3>(3, 0)), covpq.transpose(), 1e-5);
+}
+
 TEST(MSF_Core, SimilarityTransform) {
   using namespace msf_core;
 
@@ -66,61 +138,8 @@ TEST(MSF_Core, SimilarityTransform) {
   Vector3 pr = GeometryMsgsToEigen(Pd.pose.position);
   Eigen::Quaterniond qr = GeometryMsgsToEigen(Pd.pose.orientation);
 
-  EXPECT_NEAR_EIGEN(pr,  p, 1e-2);
-  EXPECT_NEAR_EIGEN(qr.coeffs(),  q.coeffs(), 1e-3);
-
-  // Xi matrix test.
-  EXPECT_NEAR_EIGEN(XiMat(q.coeffs()).transpose() * q.coeffs(),
-              (Eigen::Matrix<double, 3, 1>::Zero()), 1e-5);
-
-  // Block writing test.
-  similarity_transform::Pose::_covariance_type cov;
-  for (int r = 0; r < 6; r++)
-    for (int c = 0; c < 6; c++)
-      cov[r + c * 6] = r * c;
-  Eigen::Map<Matrix6> covm(cov.data());
-
-  EXPECT_NEAR_EIGEN(GeometryMsgsCovBlockToEigen(cov, geometry_msgs::cov::p,
-                                      geometry_msgs::cov::p),
-                    (covm.block<3, 3>(0, 0)), 1e-5);
-
-  EXPECT_NEAR_EIGEN(GeometryMsgsCovBlockToEigen(cov, geometry_msgs::cov::q,
-                                      geometry_msgs::cov::q),
-                    (covm.block<3, 3>(3, 3)), 1e-5);
-
-  EXPECT_NEAR_EIGEN(GeometryMsgsCovBlockToEigen(cov, geometry_msgs::cov::p,
-                                      geometry_msgs::cov::q),
-                    (covm.block<3, 3>(0, 3)), 1e-5);
-
-  EXPECT_NEAR_EIGEN(GeometryMsgsCovBlockToEigen(cov, geometry_msgs::cov::q,
-                                      geometry_msgs::cov::p),
-                    (covm.block<3, 3>(3, 0)), 1e-5);
-
-  Matrix3 covp;
-  covp << 31, 32, 33, 32, 34, 35, 33, 35, 36;
-  Matrix3 covq;
-  covq << 21, 22, 23, 22, 24, 25, 23, 25, 26;
-  Matrix3 covpq;
-  covpq << 1, 2, 3, 4, 5, 6, 7, 8, 9;
-
-  EigenCovBlockToGeometryMsgs(cov, covq, geometry_msgs::cov::q,
-                              geometry_msgs::cov::q);
-  EigenCovBlockToGeometryMsgs(cov, covp, geometry_msgs::cov::p,
-                              geometry_msgs::cov::p);
-
-  EigenCovBlockToGeometryMsgs(cov, covpq, geometry_msgs::cov::p,
-                              geometry_msgs::cov::q);
-
-  EigenCovBlockToGeometryMsgs(cov, covpq.transpose(), geometry_msgs::cov::q,
-                              geometry_msgs::cov::p);
-
-  EXPECT_NEAR_EIGEN((covm.block<3, 3>(0, 0)), covp, 1e-5);
-
-  EXPECT_NEAR_EIGEN((covm.block<3, 3>(3, 3)), covq, 1e-5);
-
-  EXPECT_NEAR_EIGEN((covm.block<3, 3>(0, 3)), covpq, 1e-5);
-
-  EXPECT_NEAR_EIGEN((covm.block<3, 3>(3, 0)), covpq.transpose(), 1e-5);
+  EXPECT_NEAR_EIGEN(pr, p, s_p);
+  EXPECT_NEAR_EIGEN(qr.coeffs(), q.coeffs(), s_q);
 }
 
 MSF_UNITTEST_ENTRYPOINT
