989c68c5c754256db02ee875271b558ab93e9e64
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-03-25 18:10:53 +0100

    Added Markus to header and fixed bugtracker

--- msf_simulate/src/msf_simulator.cc
+++ msf_simulate/src/msf_simulator.cc
@@ -1,8 +1,20 @@
 /*
- * MSFSimulator.cc
+ * Copyright (C) 2014 Simon Lynen, ASL, ETH Zurich, Switzerland
+ * You can contact the author at <slynen at ethz dot ch>
+ * Copyright (C) 2014 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ * You can contact the author at <acmarkus at ethz dot ch>
  *
- *  Created on: Jun 30, 2013
- *      Author: slynen
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <stdexcept>
