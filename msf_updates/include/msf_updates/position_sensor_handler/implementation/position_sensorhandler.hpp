1a12587aca2aecafbaa5114fb98bb9e1e9c82eb4
omaris <omaris@ethz.ch>
2014-11-19 13:46:22 +0100

    Renaming of standard transform input of position sensor handler...
    ... to avoid confusion with same name of pose sensor handler

--- msf_updates/include/msf_updates/position_sensor_handler/implementation/position_sensorhandler.hpp
+++ msf_updates/include/msf_updates/position_sensor_handler/implementation/position_sensorhandler.hpp
@@ -53,7 +53,7 @@ PositionSensorHandler<MEASUREMENT_TYPE, MANAGER_TYPE>::PositionSensorHandler(
   ("position_input", 20, &PositionSensorHandler::MeasurementCallback, this);
   subTransformStamped_ =
       nh.subscribe<geometry_msgs::TransformStamped>
-  ("transform_input", 20, &PositionSensorHandler::MeasurementCallback, this);
+  ("transform_position_input", 20, &PositionSensorHandler::MeasurementCallback, this);
   subNavSatFix_ =
       nh.subscribe<sensor_msgs::NavSatFix>
   ("navsatfix_input", 20, &PositionSensorHandler::MeasurementCallback, this);
