2cdf8e61a09afe52335a06d81a902ad8e3ef1448
Yannick Huber <huberya@student.ethz.ch>
2017-11-18 12:46:33 +0100

    trying to fix pipeline

--- msf_updates/Python/position_to_error_handler/pos_error_leica.py
+++ msf_updates/Python/position_to_error_handler/pos_error_leica.py
@@ -15,22 +15,23 @@ import csv
 import os
 #import sys
 #sys.path.insert(0, '~/catkin_ws/src/ethzasl_msf/msf_core')
-from sensor_fusion_comm.msg import DoubleArrayStamped as messagetype
+from geometry_msgs.msg import PoseWithCovarianceStamped as messagetype
+from sensor_fusion_comm.msg import DoubleArrayStamped as messagetypeout
 
-class PosToErrHandler:
+class PosErrLeica:
   def __init__(self):
     self.ground_truth_=rospy.get_param("~ground_truth","")
-    self.eps_=100000000.0
+    self.eps_=10000000.0
     self.csvfile_=open(os.path.join(self.ground_truth_, "data.csv"), "r")
     self.truth_reader_=list(csv.reader(self.csvfile_))
     #start reading at line 1 (i.e. second line) since first is expected to be header
+    self.transform_=np.array([0,0,0])
     self.linenr_=1
     self.nlines_=len(self.truth_reader_)
     self.init_meas_=False
-    self.transform_=np.array([0,0,0])
     
     #init publisher
-    self.pub_=rospy.Publisher("position_to_error_handler/output", messagetype, queue_size=20)
+    self.pub_=rospy.Publisher("pos_error_leica/output", messagetypeout, queue_size=20)
     
   def l2_norm(self, arrin, truth):
     return np.linalg.norm(arrin-truth, None)
@@ -46,14 +47,12 @@ class PosToErrHandler:
     if(self.init_meas_):
       #print("callback")
       timestamp=float(str(data.header.stamp))
+      #print (timestamp)
       truth=0
       while self.linenr_<self.nlines_:
         if(timestamp-self.eps_>float(self.truth_reader_[self.linenr_][0])):
-         self.linenr_+=1
+          self.linenr_+=1
         elif(timestamp+self.eps_>float(self.truth_reader_[self.linenr_][0])):
-          #truth=np.array([float(self.truth_reader_[self.linenr_][1]), float(self.truth_reader_[self.linenr_][2]),
-          #float(self.truth_reader_[self.linenr_][3]), float(self.truth_reader_[self.linenr_][8]),
-          #float(self.truth_reader_[self.linenr_][9]), float(self.truth_reader_[self.linenr_][10])])
           truth=np.array([float(self.truth_reader_[self.linenr_][1]), float(self.truth_reader_[self.linenr_][2]),
           float(self.truth_reader_[self.linenr_][3])])
           break
@@ -61,31 +60,25 @@ class PosToErrHandler:
         else:
           return
      
-      #arrin=np.array([data.data[0], data.data[1], data.data[2], data.data[3],  data.data[4],  data.data[5]])
-      arrin=np.array([data.data[0], data.data[1], data.data[2]])
+      arrin=np.array([data.pose.pose.position.x, data.pose.pose.position.y, data.pose.pose.position.z])+self.transform_
       
       #error for position
       outputp1=self.l2_norm(arrin[0:3], truth[0:3])
       outputp2=self.linf_norm(arrin[0:3], truth[0:3])
       outputp3=self.difference(arrin[0:3],truth[0:3])
       
-      #errors for velocity
-      #outputv1=self.l2_norm(arrin[3:6], truth[3:6])
-      #outputv2=self.linf_norm(arrin[3:6], truth[3:6])
-      #outputv3=self.difference(arrin[3:6],truth[3:6])
-      
       
       #outputs: l2 norm(position), inf norm(position), l2 norm(velocity), inf norm (velocity), differences (positions), differences (velocities)
-      #datanew=(outputp1, outputp2, outputv1, outputv2, outputp3[0], outputp3[1], outputp3[2], outputv3[0], outputv3[1], outputv3[2])
-      datanew=(outputp1, outputp2, outputp3[0], outputp3[1], outputp3[2])
+      datanew=(arrin[0], arrin[1], arrin[2], outputp1, outputp2, outputp3[0], outputp3[1], outputp3[2])
       
       #keep timestamp from previous data and just replace actual data
-      data.data=datanew
-      self.pub_.publish(data)
+      dataout=messagetypeout()
+      dataout.header.stamp=data.header.stamp
+      dataout.data=datanew
+      self.pub_.publish(dataout)
 
     else:
-      arrin=np.array([data.data[0], data.data[1], data.data[2]])
-      #somehow need to fit initial values i think...but how: (initial is not 0,0,0)
+      arrin=np.array([data.pose.pose.position.x, data.pose.pose.position.y, data.pose.pose.position.z])
       
       timestamp=float(str(data.header.stamp))
       #print(timestamp)
@@ -111,8 +104,9 @@ class PosToErrHandler:
           #print(timestamp)
           #self.linenr_=1
           return
+
   def listener(self):
-    topic="position_to_error_handler/input"
+    topic="pos_error_leica/input"
     rospy.Subscriber(topic, messagetype, self.callback)
     rospy.spin()
       
@@ -121,8 +115,8 @@ class PosToErrHandler:
       
 if __name__ == '__main__':
   try:
-    rospy.init_node('position_to_error_handler', anonymous=True)
-    gs=PosToErrHandler()
+    rospy.init_node('pos_error_leica', anonymous=True)
+    gs=PosErrLeica()
     gs.listener()
   except rospy.ROSInterruptException:
     pass
